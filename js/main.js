c6328ee9b86c5e6af41366afcdd984ab63d023c9
jsevcov <jan.sevcov@actum.cz>
2019-07-15 10:19:42 +0200

    pagination

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
+let from = 0;
+let to = 5;
+
 function showData() {
-    $.getJSON('https://api.github.com/users?since=10', users => {
-        console.log(users);
-        document.getElementById('datashow').innerHTML = users.map(user => {
-            return `
+  $.getJSON('https://api.github.com/users?since=10', users => {
+    console.log(users);
+    console.log(users.length);
+
+    document.getElementById('datashow').innerHTML = users.slice(from, to).map(user => {
+      return `
         <p class="mt-4"><img src=${user.avatar_url}/>
         <p><a href=/user.html?user=${user.login}>${user.login}</a>
         `;
-        });
     });
+  });
 }
 
 function userDetail() {
-    let userUrl = document.location.href;
-    let userName = userUrl.split("?user=");
-    console.log(userName[1]);
-    $.getJSON('https://api.github.com/users/' + (userName[1]), userDetail => {
-        console.log(userDetail);
-        document.getElementById('userAllData').innerHTML =  `
+  let userUrl = document.location.href;
+  let userName = userUrl.split("?user=");
+  console.log(userName[1]);
+  $.getJSON('https://api.github.com/users/' + (userName[1]), userDetail => {
+    console.log(userDetail);
+    document.getElementById('userAllData').innerHTML = `
         <p><img src=${userDetail.avatar_url}/>
         <p>user name: ${userDetail.name}
         <p>user email: ${userDetail.email}
@@ -27,5 +32,25 @@ function userDetail() {
         <p>user company: ${userDetail.company}
         <p>user blog: ${userDetail.blog}
         `;
-    });
+  });
+}
+
+function pagination() {
+
+}
+
+function nextStep() {
+  from += 5;
+  to += 5;
+  showData();
+}
+
+function previousStep() {
+  if (from === 0 && to === 5) {
+    alert("there is no more previous steps")
+  } else {
+    from -= 5;
+    to -= 5;
+    showData();
+  }
 }
